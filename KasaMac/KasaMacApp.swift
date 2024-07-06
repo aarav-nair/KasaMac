acb5846167031181a17fa5f1f82c2660902976dc
aarav-nair <aarav.nair@gmail.com>
2024-07-06 15:58:16 -0400

    Initial Commit

---
+++ KasaMac/KasaMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  KasaMacApp.swift
+//  KasaMac
+//
+//  Created by Aarav Nair on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct KasaMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
