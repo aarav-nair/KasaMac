100bb12bfbc76e5548095ee2d2a146a21e5eb84a
aarav-nair <aarav.nair@gmail.com>
2024-07-06 17:54:08 -0400

    Create PlugRowView.swift

---
+++ KasaMac/PlugRowView.swift
@@ -0,0 +1,20 @@
+//
+//  PlugRowView.swift
+//  KasaMac
+//
+//  Created by Aarav Nair on 7/6/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct PlugRowView: View {
+    var name: String = ""
+    var body: some View {
+        Text(name)
+    }
+}
+
+#Preview {
+    ContentView()
+}
