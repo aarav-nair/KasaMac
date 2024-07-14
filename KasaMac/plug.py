b73c8fad089406ce103ef1434fc7943044358bc2
aarav-nair <aarav.nair@gmail.com>
2024-07-14 17:25:35 -0400

    Update plug.py

--- KasaMac/plug.py
+++ KasaMac/plug.py
@@ -6,39 +6,34 @@ from kasa import SmartPlug
 from kasa import Discover, Credentials
 
 
-
 async def all_plugs():
    return await Discover.discover()
    
-
+   
 def findPlugs():
-   all = asyncio.run(all_plugs())
-   addresses = list(all.keys())
-   devices = list(all.values())
-   names = list(map(lambda dev: dev.alias, devices))
-   dict = dict(zip(names, addresses))
-   return dict
-   
-   
-def getPlugNames():
-    plugs = findPlugs()
-    keys = list(all.keys())
+    all = asyncio.run(all_plugs())
     devices = list(all.values())
     
+    names = list(map(lambda dev: dev.alias, devices))
+    addresses = list(all.keys())
+    isOn = list(map(lambda dev: dev.is_on, devices))
+    
+    return [names, addresses, isOn]
+
 
 def auto(address: str, lower: int, upper: int):
-   plug = SmartPlug(str)
-   percent = currPercent()
-   isCharge = isCharging()
+    plug = SmartPlug(str)
+    percent = currPercent()
+    isCharge = isCharging()
 
-   if isCharge and percent >= upper:
+    if isCharge and percent >= upper:
       asyncio.run(plug.turn_off())
 
-   elif not isCharge and percent <= lower:
+    elif not isCharge and percent <= lower:
       asyncio.run(plug.turn_on())
    
 
 if __name__ == "__main__":
-   # auto("127.0.0.1", 20, 80)
-   print(kasa.__file__)
-   findPlugs()
+    # auto("127.0.0.1", 20, 80)
+    print(kasa.__file__)
+    findPlugs()
