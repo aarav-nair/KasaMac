dc8b51386a4cee4ecb79316d27d0cd5531a138ea
aarav-nair <aarav.nair@gmail.com>
2024-07-21 15:38:32 -0400

    Update BatteryView.swift

--- KasaMac/Views/BatteryView.swift
+++ KasaMac/Views/BatteryView.swift
@@ -11,11 +11,8 @@ import SwiftUI
 struct BatteryView: View {
     @ObservedObject var viewModel: KasaViewModel
     @State private var isShown: Bool = false
-//    var helloWorldTimer = Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(ViewController.sayHello), userInfo: nil, repeats: true)
-
 
     var body: some View {
-//        @State var plugs = viewModel.plugs
         VStack {
             Toggle(isOn: $isShown, label: {
                 Text("Battery Optimizing")
@@ -28,41 +25,14 @@ struct BatteryView: View {
                     }
                 } label: {
                     Text("Plug")
-                }.pickerStyle(.menu)
-//
-//                Picker(selection: $minimum) {
-//                    ForEach([1, 2, 3], id: \.self) { $plug in
-//                        Text("\(plug)")
-//                    }
-//                } label: {
-//                    Text("Minimum Percent")
-//                }
-//
-//                Picker(selection: $viewModel.maximum) {
-//                    ForEach(Array(75...85), id: \.self) { $num in
-//                        Text(num)
-//                    }
-//                } label: {
-//                    Text("Maximum Percent")
-//                }.pickerStyle(.wheel)
-//                
-//                Picker(selection: $viewModel.target) {
-//                    ForEach(Array(70...100), id: \.self) { $num in
-//                        Text(num)
-//                    }
-//                } label: {
-//                    Text("Target Percent")
-//                }.pickerStyle(.wheel)
-//                
-                DatePicker("Please enter a time", selection: $viewModel.time, displayedComponents: .hourAndMinute)
-
-//                Picker("Plug", selection: $viewModel.currPlug) {
-//                    ForEach($plugs, id: \.self) { $plug in
-//                        Text(plug.name)
-//                    }
-//                }.pickerStyle(.menu)
-                
-                
+                }
+                .pickerStyle(.menu)
+                .onChange(of: viewModel.currPlug) {
+                    let address = viewModel.currPlug?.address
+                    Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                        optimizer(address: address!, lower: viewModel.minimum, upper: viewModel.maximum)
+                    }
+                }
             }
         }
     }
