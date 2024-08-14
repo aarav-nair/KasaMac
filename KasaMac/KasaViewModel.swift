2b668c04526976632f4a49a2f7b438bf7fdbd77f
aarav-nair <aarav.nair@gmail.com>
2024-08-13 20:41:14 -0400

    Update KasaViewModel.swift

--- KasaMac/KasaViewModel.swift
+++ KasaMac/KasaViewModel.swift
@@ -11,8 +11,8 @@ import SwiftUI
 
 class KasaViewModel: ObservableObject {
     @Published var discovered: Bool = false
-    @Published var plugs: [PlugItem] = []
-    @Published var currPlug: PlugItem?
+    @Published var plugs: [KasaRowViewModel] = []
+    @Published var currPlug: KasaRowViewModel?
     @Published var minimum: Int = 20
     @Published var maximum: Int = 80
     @Published var target: Int = 80
@@ -26,19 +26,35 @@ class KasaViewModel: ObservableObject {
         let addresses = response[1]
         let isOn = response[2]
         let len = names.count
-        var result: [PlugItem] = []
+        var result: [KasaRowViewModel] = []
 
         (0..<len).forEach { i in
             let bool = Bool("\(isOn[i])".lowercased())
-            let plug = PlugItem(name: "\(names[i])", address: "\(addresses[i])", isOn: bool ?? false)
+            let plug = KasaRowViewModel(name: "\(names[i])", address: "\(addresses[i])", isOn: bool ?? false)
             result.append(plug)
         }
         self.plugs = result
     }
 }
 
-struct PlugItem: Hashable {
-    var name: String
-    var address: String
-    var isOn: Bool
+class KasaRowViewModel: ObservableObject, Hashable {
+    let id: UUID
+    @Published var name: String
+    @Published var address: String
+    @Published var isOn: Bool
+    
+    init(name: String, address: String, isOn: Bool) {
+        self.id = UUID()
+        self.name = name
+        self.address = address
+        self.isOn = isOn
+    }
+    
+    static func == (lhs: KasaRowViewModel, rhs: KasaRowViewModel) -> Bool {
+            return lhs.id == rhs.id
+        }
+        
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
 }
